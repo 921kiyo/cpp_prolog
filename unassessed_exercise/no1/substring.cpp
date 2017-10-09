60eb09083b14064feba48d44a3c74b318ca2bbec
Kiyohito Kunii <kk3317@voxel07.doc.ic.ac.uk>
2017-10-09 10:43:10 +0100

    ad no1

---
+++ unassessed_exercise/no1/substring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+
+bool is_prefix(string first_string, string second_string){
+}
+
+int substring_position(string first, string second){
+  
+}
---
+++ unassessed_exercise/no1/substring.h
@@ -0,0 +1,12 @@
+#ifndef SUBSTRING_H
+#define SUBSTRING_H
+
+bool is_prefix(string first, string second){
+  
+}
+
+int substring_position(string first, string second){
+  
+}
+
+#endif
