3781971e2999c31b7bb64e63e5360de2cddd7b90
Kiyohito Kunii <kk3317@voxel06.doc.ic.ac.uk>
2017-10-10 21:37:32 +0100

    working on substring

--- unassessed_exercise/no1/substring.cpp
+++ unassessed_exercise/no1/substring.cpp
@@ -5,11 +5,34 @@ using namespace std;
 
 const int MAX = 80;
 
-bool is_prefix(char first_string, char second_string){
-  
+bool is_prefix(char first_string[], char second_string[]){
+  if(strlen(first_string) == 0){
+    return true;
+  }
+  else if(strlen(second_string) == 0 && strlen(first_string) > 0){
+    return false;
+  }
+  while()
+  if(first_string != second_string){
+    return false;
+  }
+  if(is_prefix(first_string[]))
 }
 
-int substring_position(char first[], char second[]){
-  int count = 0; 
-  while(first[count] != '\0' || second[count] != '\0')
+int substring_position(char first[], char second[], int position){
+  int i = 0;
+  while(second[i] != '\0'){
+    if(first[count] == second[i]){
+      if(is_prefix(first, second, i)){
+	return i;
+      }
+      else{
+	return -1;
+      }
+    }
+    else{
+      return -1;
+    }
+    i++;
+  }
 }
