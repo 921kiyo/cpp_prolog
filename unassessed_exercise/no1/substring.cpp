29e5bb254997c62630be2b04c418639d3b76eec2
921kiyo <kiyo921@hotmail.co.jp>
2017-10-11 07:45:26 +0100

    working on substring

--- unassessed_exercise/no1/substring.cpp
+++ unassessed_exercise/no1/substring.cpp
@@ -1,38 +1,47 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 
-const int MAX = 80;
-
 bool is_prefix(char first_string[], char second_string[]){
+  //cout << "A" << strlen(first_string);
   if(strlen(first_string) == 0){
     return true;
   }
   else if(strlen(second_string) == 0 && strlen(first_string) > 0){
     return false;
   }
-  while()
-  if(first_string != second_string){
-    return false;
+
+  if(is_prefix((first_string + 1),(second_string + 1))){
+    return true;
   }
-  if(is_prefix(first_string[]))
+  else{
+    for(int i = 0; first_string[i] != '\0'; i++ ){
+      if(second_string[i] != first_string[i]){
+	return false;
+      }
+    }
+  }
+
 }
 
-int substring_position(char first[], char second[], int position){
-  int i = 0;
-  while(second[i] != '\0'){
-    if(first[count] == second[i]){
-      if(is_prefix(first, second, i)){
+int substring_position(char first[], char second[]){
+  //cout << "AA" << first;
+  //cout << "BB" << second + 1;
+  if(strlen(first) == 0){
+    return 0;
+  }
+  if(strlen(second) == 0){
+    return -1;
+  }
+
+  else{
+  
+    for (int i = 0; second[i] != '\0'; i++){
+      if(is_prefix(first, second+1)){
 	return i;
       }
-      else{
-	return -1;
-      }
     }
-    else{
-      return -1;
-    }
-    i++;
   }
 }
--- unassessed_exercise/no1/substring.h
+++ unassessed_exercise/no1/substring.h
@@ -1,12 +1,8 @@
 #ifndef SUBSTRING_H
 #define SUBSTRING_H
 
-bool is_prefix(string first, string second){
-  
-}
+bool is_prefix(char first[], char second[]);
 
-int substring_position(string first, string second){
-  
-}
+int substring_position(char first[], char second[]);
 
 #endif
