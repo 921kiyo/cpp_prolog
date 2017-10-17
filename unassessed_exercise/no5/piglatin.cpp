e3a56b1dca98d8a238622a36c6b0a936aeb7c4e3
921kiyo <kiyo921@hotmail.co.jp>
2017-10-17 17:19:14 +0100

    working on no5

--- unassessed_exercise/no5/piglatin.cpp
+++ unassessed_exercise/no5/piglatin.cpp
@@ -34,13 +34,15 @@ void translateWord(const char* english, char* piglatin){
   int length = strlen(english);
   while(english != '\0'){
     // Check if the word begins with a vowel
-    if(findFirstVowel(english) == 0){
+    int vowel_position = findFirstVowel(english);
+    if(vowel_position == 0){
       strcpy(piglatin, english);
       strcat(piglatin, "way");
       break;
     }
     // When a vowel is found anywhere other than the index 0
-    else if(findFirstVowel(english) > 0){
+    else if(vowel_position > 0){
+      move_character_before(english, piglatin, vowel_position);
       break;
     }
     // When no vowel is found
@@ -51,4 +53,24 @@ void translateWord(const char* english, char* piglatin){
     }
     english++;
   }
+}
+
+void move_character_before(const char* english, char* piglatin, int position){
+  cout << english << endl;
+  cout << piglatin << endl;
+  int length = strlen(english);
+
+  int x = 0;
+  for(int i = position; english[i] != '\0'; i++){
+    piglatin[x] = english[i];
+    x++;
+  }
+  
+  for(int i = 0; i < position; i++){
+    piglatin[x] = english[i];
+    x++;
+  }
+
+  strcat(piglatin, "ay");
+
 }
--- unassessed_exercise/no5/piglatin.h
+++ unassessed_exercise/no5/piglatin.h
@@ -7,4 +7,6 @@ bool check_vowels(char word, int position, int index);
 
 void translateWord(const char* english, char* piglatin);
 
+void move_character_before(const char* english, char* piglatin, int position);
+
 #endif
