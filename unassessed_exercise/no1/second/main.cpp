69e22da28a8d0f3600ad5cee5ed4e3026d53040e
Kiyohito Kunii <kk3317@point47.doc.ic.ac.uk>
2017-10-20 17:00:01 +0100

    substring 2nd

--- unassessed_exercise/no1/second/main.cpp
+++ unassessed_exercise/no1/second/main.cpp
@@ -4,13 +4,15 @@
 using namespace std;
 
 int main() {
-  cout << substring_position("this", "this is a simple exercise") << endl;
-  cout << substring_position("is", "this is a simple exercise") << endl;
+  // cout << substring_position("this", "this is a simple exercise") << endl;
+  // cout << substring_position("is", "this is a simple exercise") << endl;
   cout << substring_position("is a", "this is a simple exercise") << endl;
-  cout << substring_position("is an", "this is a simple exercise") << endl;
-  cout << substring_position("exercise", "this is a simple exercise") << endl;
-  cout << substring_position("simple exercise", "this is a simple") << endl;
-  cout << substring_position("", "this is a simple exercise") << endl;
-  cout << substring_position("", "") << endl;
+  // cout << substring_position("is an", "this is a simple exercise") << endl;
+  // cout << substring_position("exercise", "this is a simple exercise") << endl;
+  // cout << substring_position("simple exercise", "this is a simple") << endl;
+  // cout << substring_position("", "this is a simple exercise") << endl;
+  // cout << substring_position("", "") << endl;
+  // cout << is_prefix("indent", "indentation") << endl;
+  // cout << is_prefix("indentationaa", "indentation") << endl;
   return 0;
 }
--- unassessed_exercise/no1/second/substring.h
+++ unassessed_exercise/no1/second/substring.h
@@ -1,4 +1,8 @@
 #ifndef SUBSTRING_H
 #define SUBSTRING_H
 
+int substring_position(const char* first, const char* second);
+
+bool is_prefix(const char* first, const char* second);
+
 #endif
