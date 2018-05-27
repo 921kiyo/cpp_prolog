fafcb1b60bf706770de6019a31ac02567f1b5c71
Kiyohito Kunii <kk3317@line05.doc.ic.ac.uk>
2018-05-27 16:45:26 +0100

    update

---
+++ cpp/practice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void quick_sort(int list[], int left, int right){
+  int pivot, left_arrow, right_arrow;
+}
+
+int main(){
+  char sentence[]="hello world";
+  no_repetition(sentence);
+  return 0;
+}
